e1019f421e406ce74032d54762470410fee9f406
Anouska Gievano <nooska44@gmail.com>
2022-05-04 18:18:56 +0800

    got the saving into local storage to work

--- js/main.js
+++ js/main.js
@@ -21,15 +21,72 @@
 //     // document.querySelector('#count').innerText = (amount.length);
 // }
 
+// //incase I mess up 
+// document.addEventListener('change', addAmount)
 
-//incase I mess up 
-document.addEventListener('change', addAmount)
-
-function addAmount(){
+// function addAmount(){
  
+//     const amount = document.querySelectorAll('input[type="checkbox"]:checked');
+//     document.querySelector('#count').innerText = (amount.length);
+// }
+
+
+
+
+
+
+
+let keys = Object.keys(localStorage)  
+
+for(let i = 0; i < keys.length; i++ ){
+    let element = document.getElementById(keys[i])
+    if (element) {
+        element.checked = true
+    }
+}
+
+
+
+document.addEventListener('change', updateCount)
+
+function updateCount(event){
     const amount = document.querySelectorAll('input[type="checkbox"]:checked');
     document.querySelector('#count').innerText = (amount.length);
+    if(event.target.checked){
+        localStorage.setItem(event.target.id, 'true')
+    } else {
+        localStorage.removeItem(event.target.id, 'false')
+    }
+    
+    console.log(event.target.id)
 }
+
+
+document.getElementById("clear").onclick = clear_me;
+
+function clear_me() {
+    localStorage.clear();
+   
+}
+
+//add document.addEventListener('click', clear)
+
+// uncheck all checked boxes 
+
+
+
+
+
+
+
+
+document.getElementById('clear')
+
+// document.addEventListener('click', clear)
+
+// function clear(){
+//     document.querySelector('button').localStorage.clear()
+// }
 
 
 
