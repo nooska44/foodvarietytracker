8aa011149dd3b7fb7ed54268d69822c031147386
Anouska Gievano <nooska44@gmail.com>
2022-05-05 18:50:32 +0800

    updated tp add a new about page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // check the boxes that are stored in local storage
 let keys = Object.keys(localStorage)  
+// let count = Object.keys(localStorage).length
 
+
+
+console.log(count)
 for(let i = 0; i < keys.length; i++ ){
     let element = document.getElementById(keys[i])
     if (element) {
         element.checked = true
     }
+    document.querySelector('#count').innerText = (keys.length);
 }
 
 //count as checkboxes are checked and unchecked
@@ -30,9 +35,11 @@ document.querySelector('button').addEventListener('click', clearAll)
 
  function clearAll() {
     localStorage.clear()
+    
     for (const checkbox of document.querySelectorAll('.box')) {
         checkbox.checked = false //for unselection
         }
+        console.log(document.querySelector('#count').innerText = 0)
     }; 
 
 
